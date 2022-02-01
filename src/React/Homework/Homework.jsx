e6c0e0274d3f4f283f501158d3609b396d99d097
Bianca Bonilla <biancabonilla10@yahoo.com>
2022-01-31 19:52:36 -0800

    Styling Main

--- src/React/Homework/Homework.jsx
+++ src/React/Homework/Homework.jsx
@@ -7,6 +7,7 @@ import Essays from './Essays/Essays.jsx';
 const Homework = () => { 
     return (
         <HomeworkStyled>
+            <h1>Homework</h1>
             <SunAndMoon />
             <Essays />
         </HomeworkStyled>
@@ -16,7 +17,6 @@ const Homework = () => {
 export default Homework;
 
 const HomeworkStyled = styled.div`
-    background-color: gray;
-    padding: 20px;
+ 
     `;
 
