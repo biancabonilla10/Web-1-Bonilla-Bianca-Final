9d71d99bc3f318c282f986d48dbefaed37e969eb
Bianca Bonilla <biancabonilla10@yahoo.com>
2022-03-07 19:11:50 -0800

    Slideshow: Implimented Carousel Library

--- src/React/Welcome/Slideshow/Slideshow.jsx
+++ src/React/Welcome/Slideshow/Slideshow.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+
+
 import { slidesData } from './slidesData.js';
 
 import Slide from './Slide.jsx';
@@ -8,13 +13,16 @@ import Slide from './Slide.jsx';
 const Slideshow = () => {
     return (
         <SlideshowStyled className='Slideshow'>
-            Slideshow 
-
+            <Carousel
+                autoPlay
+                infiniteLoop
+            >
             {
                 slidesData.map((slide, idx) => {
                     return <Slide key={ idx } slide={ slide }  />
                 })
             }
+            </Carousel>
         </SlideshowStyled>
     );
 }
@@ -23,4 +31,7 @@ export default Slideshow;
 
 const SlideshowStyled = styled.div`
     
+    .carousel .slide .legend {
+        background-color: maroon;
+    }
 `;
